bc2c50899631d5de96987bd6d8f4efa501ecaa7d
bimmbim <bimabaraja76@gmail.com>
2025-05-15 14:46:57 +0700

    commit12

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -104,7 +104,14 @@ void traverse() {
         }
     Node *currentNode = START;
 
+    cout << "\nRecords in ascending order of roll number are:\n";
+    int i = 0;
+    while (currentNode != NULL) {
+        cout << i + 1 << ". " << currentNode -> noMhs << " " << endl;
 
+        currentNode = currentNode -> next;
+        i++;
+        }
     }
 
 
