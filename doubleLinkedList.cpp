534546eabb0ce207235e34e6b5e013dfab4f30ef
bimmbim <bimabaraja76@gmail.com>
2025-05-15 14:46:05 +0700

    commit7

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -61,7 +61,17 @@ class DoubleLinkedList {
         current -> next = newNode;
     }
     
+    void hapus() {
+        if (START == NULL) {
+            cout << "\nList is empty" << endl;
+            return;
+        }
 
+
+
+
+
+    }
 
 
 
